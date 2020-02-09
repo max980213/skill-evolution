f3813cc63f28a9fcf80261da0b66498c03179aa4
max980213 <49967356+max980213@users.noreply.github.com>
2020-02-09 21:35:39 +0800

    Update 素数.cpp

--- 素数.cpp
+++ 素数.cpp
@@ -1,13 +1,36 @@
-//100内的素数
+//求素数的两种方法（以100以内素数为例）
+
+//1.直接法：若存在n%a==0，则n不是素数
+//          但考虑到，若n%a==0，则n/a=m（能除尽），则n%m==0，且a和n/a一定在根号n的左右两侧
+//          所以在枚举时，只需要判断从2到根号n，有没有能除尽的数即可
 
 #include <cstdio>
-
+#include <math.h>
 #define MAX 101
+
+bool isPrime(int n)  //复杂度：判断该数为O(sqrt(n))
+{
+    if(n<=1)
+        return false;
+    int sqr=(int)sqrt(n);
+    for(int i=2;i<=sqr;i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+//2.埃氏筛法
+//思想：对每一个素数，筛掉它所有的倍数
+//如：2是素数，筛掉4,6,8,......
+//    3没有被之前的步骤筛去，所以3是素数，筛掉6,9,12.....
+//    重复以上过程，从小到大到达某数a，如果a没有被筛掉，则它一定是素数
 
 int prime[MAX],pNum=0;
 bool p[MAX]={0};
 
-void find_prime()
+void find_prime()    //复杂度为O(loglogn)
 {
     for(int i=2;i<MAX;i++)  //1不是素数
     {
